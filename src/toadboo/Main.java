c246535efcd4b8ae4a354f5eba3dc5228cc125e8
ElizaAldana <eliza.andrade99@gmail.com>
2020-09-06 21:16:43 -0500

    movimiento 3 boo y toad

--- src/toadboo/Main.java
+++ src/toadboo/Main.java
@@ -47,7 +47,7 @@ public class Main extends PApplet {
 	int time;
 	int second;
 	
-	//Matrix?
+	//Matrix
 	int gameMatrixX;
 	int gameMatrixY;
 	
@@ -71,6 +71,8 @@ public class Main extends PApplet {
 	int cherryMy;
 	int cherryPosX;
 	int cherryPosY;
+	
+	boolean moveBoo;
 	
 	//matrix board
 	int[][] mb = {
@@ -96,6 +98,7 @@ public class Main extends PApplet {
 			{9,9,9,9,9,9,9,9,9,9,9,9,9,9},
 			};
 			
+	int timer;
 	
 	
 	public static void main(String[] args) {
@@ -119,12 +122,12 @@ public class Main extends PApplet {
 		grape = loadImage("images/1x/grape.png");
 		cherry = loadImage("images/1x/cherry.png");
 
-		
+		frameRate(10);
 		//toad position
 		toad = new Toad (posXtoad, posYtoad, this);
 		//booaka position
 		booaka = new BooAka (posXbooaka, posYbooaka, this);
-		//booaka position
+		//booao position
 		booao = new BooAo (posXbooao, posYbooao, this);
 		//boomidori position
 		boomidori = new BooMidori (posXboomidori, posYboomidori, this);
@@ -142,8 +145,81 @@ public class Main extends PApplet {
 		//visualize time
 		time=0;
 		second=0;
+		timer = 0;
 		
+		//Matrix
+		for (int i = 0; i < row ; i++) {
+			for (int j = 0; j < col ; j++) {
+				//Toad Paint
+				if(mb[i][j] == 1) {
+					gameMatrixX = i;
+					gameMatrixY = j;
+					toad.setMatrixX(gameMatrixX);
+					toad.setMatrixX(gameMatrixY);
+					posXtoad = gameMatrixX*50+25;
+					posYtoad = gameMatrixY*50+25;
+				toad = new Toad (posXtoad, posYtoad, this);
+				}
+				//BooMidori Paint
+				if(mb[i][j] == 6) {
+					booMidoriMx = i;
+					booMidoriMy = j;
+					boomidori.setMatrixX(booMidoriMx);
+					boomidori.setMatrixX(booMidoriMy);
+					posXboomidori = booMidoriMx*50+25;
+					posYboomidori = booMidoriMy*50+25;
+					boomidori = new BooMidori (posXboomidori, posYboomidori, this);
+				}
+				//BooOrenji Paint
+				if(mb[i][j] == 5) {
+					booOrenjiMx = i;
+					booOrenjiMy = j;
+					booorenji.setMatrixX(booOrenjiMx);
+					booorenji.setMatrixX(booOrenjiMy);
+					posXbooorenji = booOrenjiMx*50+25;
+					posYbooorenji = booOrenjiMy*50+25;
+					booorenji = new BooOrenji (posXbooorenji, posYbooorenji, this);
+				}
+				//BooAo paint
+				if(mb[i][j] == 4) {
+					booAoMx = i;
+					booAoMy = j;
+					booao.setMatrixX(booAoMx);
+					booao.setMatrixX(booAoMy);
+					posXbooao = booAoMx*50+25;
+					posYbooao = booAoMy*50+25;
+					booao = new BooAo (posXbooao, posYbooao, this);
+				}
+				//BooAka paint
+				if(mb[i][j] == 3) {
+					booAkaMx = i;
+					booAkaMy = j;
+					booaka.setMatrixX(booAkaMx);
+					booaka.setMatrixX(booAkaMy);
+					posXbooaka = booAkaMx*50+25;
+					posYbooaka = booAkaMy*50+25;
+					booaka = new BooAka (posXbooaka, posYbooaka, this);
+				}
+				//Grape paint
+				if(mb[i][j] == 7) {
+					grapeMx = i;
+					grapeMy = j;
+					grapePosX = grapeMx*50+25;
+					grapePosY = grapeMy*50+25;
+				}
+				//Cherry paint
+				if(mb[i][j] == 8) {
+					cherryMx = i;
+					cherryMy = j;
+					cherryPosX = cherryMx*50+25;
+					cherryPosY = cherryMy*50+25;
+				}
+			}
+		}
 		
+		moveBoo = true;
+
+	}
 	
 	public void draw () {
 		
@@ -168,12 +244,21 @@ public class Main extends PApplet {
 			//time
 			text(time,500,64);
 			second = second + 1;
-			if (second == 100) {
+			if (second == 10) {
 				second = 0;
 			}
-			if (screen == 2 && second == 99) {
+			if (screen == 2 && second == 9) {
 				time=time + 1;
 			}
+			
+			
+			
+			
+			//Winning Page
+			if(scoreGame == 14500) {
+				screen = 3;
+			}
+			
 			
 			//Matrix game
 			for (int i = 0; i < row; i++) {
@@ -182,14 +267,64 @@ public class Main extends PApplet {
 					if (mb [i][j] == 1) {
 						toad.loadImage();
 						}
-					//booaka Image
+					
+					//Booaka Image
 					if (mb [i][j] == 3) {
 						booaka.loadImage();
-						}
-					//booaka Image
+						int a = (int) random(0,20);
+						if(a > 15) {
+							if(mb[booAkaMx][booAkaMy - 1] == 0 && moveBoo == true) {	
+								booaka.moveUp();
+								booAkaMy = booAkaMy - 1;
+								} else if(mb[booAkaMx][booAkaMy - 1] == 0 && moveBoo == true) {
+								booaka.moveDown();
+								booAkaMy = booAkaMy + 1;
+								}else if(mb[booAkaMx + 1][booAkaMy] == 0 && moveBoo == true) {
+								booaka.moveRight();
+								booAkaMx = booAkaMx + 1;
+								}else if(mb[booAkaMx - 1][booAkaMy] == 0 && moveBoo == true) {
+								booaka.moveLeft();
+								booAkaMx = booAkaMx - 1;
+									}
+									
+							}
+
+					}
+					
+					//Booao Image
 					if (mb [i][j] == 4) {
 						booao.loadImage();
+							int booRandomMoveFast = (int) random(0,4);	
+							switch(booRandomMoveFast) {
+							case 0:
+								if(mb[booAoMx][booAoMy - 1] != 9 && moveBoo == true) {	
+									booao.moveUp();
+									booAoMy = booAoMy - 1;
+								}
+								break;
+							case 1:
+								if(mb[booAoMx][booAoMy + 1] != 9 && moveBoo == true) {
+									booao.moveDown();
+									booAoMy = booAoMy + 1;
+
+								}
+								break;
+							case 2:
+								if(mb[booAoMx + 1][booAoMy] != 9 && moveBoo == true) {
+									booao.moveRight();
+									booAoMx = booAoMx + 1;
+								}
+								break;
+							case 3:
+								if(mb[booAoMx - 1][booAoMy] != 9 && moveBoo == true) {
+									booao.moveLeft();
+									booAoMx = booAoMx - 1;
+								}
+								break;
+							}
 						}
+					
+				//Asigned numbers on the matrix	
 					//boomidori Image
 					if (mb [i][j] == 6) {
 						boomidori.loadImage();
@@ -208,11 +343,18 @@ public class Main extends PApplet {
 						}
 					//coin Image
 					if(mb[i][j]== 2) {
-						image(coin,i*50+25,j*50+25);
-					
+						image(coin,i*50+25,j*50+25);	
 					}
 				}
 			}
+			break;
+		case 3:
+			imageMode(CENTER);
+			image(youwin,width/2,height/2);
+			break;
+		case 4:
+			imageMode(CENTER);
+			image(youlose,width/2,height/2);
 			break;
 		}
 		
@@ -248,20 +390,27 @@ public class Main extends PApplet {
 				time = 0;
 		}
 		break;
+		case 3:
+			if (mouseX>904 && mouseY>30 && mouseX<944 && mouseY<69) {
+				screen = 0;
+				score = new Score(scoreGame,this);
+				second = 0;
+				time = 0;
+		}
+			break;
+		case 4:
+			if (mouseX>904 && mouseY>30 && mouseX<944 && mouseY<69) {
+				screen = 0;
+				score = new Score(scoreGame,this);
+				second = 0;
+				time = 0;
+		}
+			break;
 		}
 	}
 	public void keyPressed() {
 		if(keyCode==UP) {
-			//System.out.println(toad.getMatrixY());
-			System.out.println(mb[gameMatrixX][gameMatrixY]);
-			if(mb[gameMatrixX][gameMatrixY - 1] == 2 ||
-					mb[gameMatrixX][gameMatrixY - 1] == 0 ||
-					mb[gameMatrixX][gameMatrixY - 1] == 3 ||
-					mb[gameMatrixX][gameMatrixY - 1] == 4 ||
-					mb[gameMatrixX][gameMatrixY - 1] == 5 ||
-					mb[gameMatrixX][gameMatrixY - 1] == 6 ||
-					mb[gameMatrixX][gameMatrixY - 1] == 7 ||
-					mb[gameMatrixX][gameMatrixY - 1] == 8) {
+			if(mb[gameMatrixX][gameMatrixY - 1] != 9) {
 				
 				toad.moveUp();
 				gameMatrixY = gameMatrixY - 1;
@@ -271,18 +420,22 @@ public class Main extends PApplet {
 					mb[gameMatrixX][gameMatrixY] = 0;
 					scoreGame = scoreGame + 100;
 				}
+				
+				if(mb[gameMatrixX][gameMatrixY] == 8) {
+					mb[gameMatrixX][gameMatrixY] = 0;
+					moveBoo = false;
+				}
 			}
+			
+			//CopyCat MoveUp Boo
+			if(mb[booMidoriMx][booMidoriMy - 1] != 9 && moveBoo == true) {
+				boomidori.moveUp();
+				booMidoriMy = booMidoriMy - 1;
+			}
+			
 		}
 		if(keyCode == DOWN) {
-			if(mb[gameMatrixX][gameMatrixY + 1] == 2 ||
-					mb[gameMatrixX][gameMatrixY + 1] == 1 ||
-					mb[gameMatrixX][gameMatrixY + 1] == 0 ||
-					mb[gameMatrixX][gameMatrixY + 1] == 3 ||
-					mb[gameMatrixX][gameMatrixY + 1] == 4 ||
-					mb[gameMatrixX][gameMatrixY + 1] == 5 ||
-					mb[gameMatrixX][gameMatrixY + 1] == 6 ||
-					mb[gameMatrixX][gameMatrixY + 1] == 7 ||
-					mb[gameMatrixX][gameMatrixY + 1] == 8) {
+			if(mb[gameMatrixX][gameMatrixY + 1] != 9) {
 				toad.moveDown();
 				gameMatrixY = gameMatrixY + 1;
 				
@@ -291,17 +444,20 @@ public class Main extends PApplet {
 					mb[gameMatrixX][gameMatrixY] = 0;
 					scoreGame = scoreGame + 100;
 				}
+				if(mb[gameMatrixX][gameMatrixY] == 8) {
+					mb[gameMatrixX][gameMatrixY] = 0;
+					moveBoo = false;
+				}
+			}
+			
+			//CopyCat MoveDown Boo
+			if(mb[booMidoriMx][booMidoriMy + 1] != 9 && moveBoo == true) {
+				boomidori.moveDown();
+				booMidoriMy = booMidoriMy + 1;
 			}
 		}
 		if(keyCode == RIGHT) {
-			if(mb[gameMatrixX + 1][gameMatrixY] == 2 ||
-					mb[gameMatrixX + 1][gameMatrixY] == 0 ||
-					mb[gameMatrixX + 1][gameMatrixY] == 3 ||
-					mb[gameMatrixX + 1][gameMatrixY] == 4 ||
-					mb[gameMatrixX + 1][gameMatrixY] == 5 ||
-					mb[gameMatrixX + 1][gameMatrixY] == 6 ||
-					mb[gameMatrixX + 1][gameMatrixY] == 7 ||
-					mb[gameMatrixX + 1][gameMatrixY] == 8) {
+			if(mb[gameMatrixX + 1][gameMatrixY] != 9) {
 				toad.moveRight();
 				gameMatrixX = gameMatrixX + 1;
 				
@@ -310,19 +466,20 @@ public class Main extends PApplet {
 					mb[gameMatrixX][gameMatrixY] = 0;
 					scoreGame = scoreGame + 100;
 				}
+				if(mb[gameMatrixX][gameMatrixY] == 8) {
+					mb[gameMatrixX][gameMatrixY] = 0;
+					moveBoo = false;
+				}
+			}
+			
+			//CopyCat MoveRight Boo
+			if(mb[booMidoriMx + 1][booMidoriMy] != 9 && moveBoo == true) {
+				boomidori.moveRight();
+				booMidoriMx = booMidoriMx + 1;
 			}
 		}
 		if(keyCode == LEFT) {
-			if(mb[gameMatrixX - 1][gameMatrixY] == 2 || 
-					mb[gameMatrixX - 1][gameMatrixY] == 1 ||
-					mb[gameMatrixX - 1][gameMatrixY] == 0 ||
-					mb[gameMatrixX - 1][gameMatrixY] == 3 ||
-					mb[gameMatrixX - 1][gameMatrixY] == 4 ||
-					mb[gameMatrixX - 1][gameMatrixY] == 5 ||
-					mb[gameMatrixX - 1][gameMatrixY] == 6 ||
-					mb[gameMatrixX - 1][gameMatrixY] == 7 ||
-					mb[gameMatrixX - 1][gameMatrixY] == 8
-					) {
+			if(mb[gameMatrixX - 1][gameMatrixY] != 9) {
 				toad.moveLeft();
 				gameMatrixX = gameMatrixX - 1;
 				
@@ -331,8 +488,18 @@ public class Main extends PApplet {
 					mb[gameMatrixX][gameMatrixY] = 0;
 					scoreGame = scoreGame + 100;
 				}
+				
+				if(mb[gameMatrixX][gameMatrixY] == 8) {
+					mb[gameMatrixX][gameMatrixY] = 0;
+					moveBoo = false;
+				}
 			}
 			
+			//CopyCat MoveLeft Boo
+			if(mb[booMidoriMx - 1][booMidoriMy] != 9 && moveBoo == true) {
+				boomidori.moveLeft();
+				booMidoriMx = booMidoriMx - 1;
+			}
 		}
 	}	
 	
